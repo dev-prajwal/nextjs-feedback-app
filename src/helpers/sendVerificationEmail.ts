666a97997ab74e4e695cc8b52b981d6fa55bd4e0
dev-prajwal <prajwaldessai@gmail.com>
2024-06-14 17:44:16 +0530

    signup and custom otp setup

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'prajwaldessai@gmail.com',
+            to: email,
+            subject: 'Next js feedback test app verification code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        })
+        return { success: true, message: 'Verification email sent successfully.' };
+    } catch (error) {
+        console.error('error sending verification email:', error);
+        return { success: false, message: 'Failed to send verification email.' };
+    }
+}
